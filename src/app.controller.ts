f4e6708af6001062553148b744dc62e83c9b7a0d
Aram Hayrapetyan <hayrapetyanaram0121@gmail.com>
2021-06-24 18:59:49 +0400

    user and item

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,44 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
 import { AppService } from './app.service';
+import { UsersService } from './users/users.service';
+import { ItemsService } from './items/items.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private appService: AppService,
+    private authService: AuthService,
+    private userService: UsersService,
+    private itemService: ItemsService,
+  ) {}
 
-  @Get()
-  getHello(): string {
+  @Get('message')
+  getHello(): { message: string } {
     return this.appService.getHello();
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return await this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  async getProfile(@Request() req) {
+    return await this.userService.findOne(req.user.email);
+  }
+
+  @Get('users')
+  async getUsers(@Request() req) {
+    return await this.userService.getAll();
+  }
+
+  @Get('items')
+  async getItems(@Request() req) {
+    return await this.itemService.getAll();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,18 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ItemsModule } from './items/items.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '../..', 'wood-ard-client/build')
-    })
+    }),
+    AuthModule,
+    UsersModule,
+    ItemsModule
   ],
   controllers: [AppController],
   providers: [AppService],
